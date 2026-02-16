2f6b637dab8643ccb0048a5927d26396a5bef4c9
anthony-r-samudio <asf.framework@proton.me>
2026-02-16 23:51:43 +0100

    Add constraint configuration

---
+++ aosl/config.py
@@ -0,0 +1,14 @@
+CONSTRAINTS = [
+    "C1_format",
+    "C2_instruction",
+    "C3_consistency",
+    "C4_completeness",
+    "C5_traceability",
+    "C6_logical",
+    "C7_definitions",
+    "C8_quantifiers",
+    "C9_dependencies",
+    "C10_epistemic",
+]
+
+DEFAULT_WEIGHT = 1.0
